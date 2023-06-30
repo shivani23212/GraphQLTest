5cc505b7f8cc15db4f67befc82f48e27f422e2df
shivani23212 <shivani23212@gmail.com>
2023-06-30 10:26:28 +0100

    Added enum of Nationality, Movie type and movie and favouriteMovies resolvers

--- schema/resolvers.js
+++ schema/resolvers.js
@@ -1,14 +1,33 @@
-const {UserList} = require("../data")
+const {UserList, MovieList} = require("../data");
+const _ = require("lodash"); // used to look for item in list
 
 const resolvers = { // all resolver functions that exist for the Query type
     Query: {
         users() {
             // make API call to db to get info about users or
             return UserList;
-
-
+        },
+        user(parent, args) {
+            const id = args.id;
+            const user = _.find(UserList, {id: Number(id)}); // find id with the value of what we entered in as the user
+            return user;
+        },
+        movies() {
+            return MovieList;
+        },
+        movie(parent, args) {
+            const name = args.name; // args.name refers to name value passed in by user
+            const movie = _.find(MovieList, {name: name});
+            return movie;
         }
 
+    },
+
+    User: {
+        favouriteMovies() {
+            return _.filter(MovieList, (movie) => movie.yearOfPublication >= 2000
+            && movie.yearOfPublication <=2010);
+        }
     }
 }
 
--- schema/type-defs.js
+++ schema/type-defs.js
@@ -7,12 +7,34 @@ const typeDefs = gql `
         name: String!, # required field
         username: String!,
         age: Int!,
-        nationality: String!
+        nationality: Nationality!,
+        friends: [User]
+        favouriteMovies: [Movie];
+    }
+
+    type Movie {
+        id: ID!,
+        name: String!,
+        yearOfPublication: Int!,
+        isInTheaters: Boolean!
     }
 
     type Query { # all GQL schemas must have this type
         users: [User!]!, # at least empty list returned
+        user(id: ID!): User!,
+        movies: [Movie!]!,
+        movie(name: String!): Movie!
+    }
+
+    enum Nationality {
+        CANADA
+        BRAZIL
+        INDIA
+        GERMANY
+        CHILE
     }
 `;
+
+
 
 module.exports = {typeDefs};
